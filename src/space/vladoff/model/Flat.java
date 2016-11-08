a13b16f76160aba35c70c5efbb0e199923d92bef
LalkaNumberOne <vexned@gmail.com>
2016-11-08 23:21:07 +0700

    Added Flat inherited from RealEstate

---
+++ src/space/vladoff/model/Flat.java
@@ -0,0 +1,11 @@
+package space.vladoff.model;
+
+/**
+ * Created by Vladislav Russinovich on 08.11.2016.
+ * NSTU, Faculty of Automation and Computer Engineering, AVT-512
+ * Licensed under WTFPL
+ */
+public class Flat extends RealEstate {
+    private PlanningType plan;
+
+}
