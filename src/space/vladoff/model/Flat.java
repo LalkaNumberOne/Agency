7b91e60a9b40d4cb7196454fe771a1742f495e22
LalkaNumberOne <vexned@gmail.com>
2016-11-08 23:30:46 +0700

    Added Constructor

--- src/space/vladoff/model/Flat.java
+++ src/space/vladoff/model/Flat.java
@@ -1,5 +1,7 @@
 package space.vladoff.model;
 
+import java.time.LocalDate;
+
 /**
  * Created by Vladislav Russinovich on 08.11.2016.
  * NSTU, Faculty of Automation and Computer Engineering, AVT-512
@@ -8,4 +10,13 @@ package space.vladoff.model;
 public class Flat extends RealEstate {
     private PlanningType plan;
 
+
+    public Flat(LocalDate dateOfRecord, int objectNumber, Person owner, int roomCount, Adress adress, MaterialType objectMaterial, int floor, double area, boolean balcony, boolean isIsolate, PlanningType plan) {
+        super(dateOfRecord, objectNumber, owner, roomCount, adress, objectMaterial, floor, area, balcony, isIsolate);
+        this.plan = plan;
+    }
+
+    public Flat(PlanningType plan) {
+        this.plan = plan;
+    }
 }
